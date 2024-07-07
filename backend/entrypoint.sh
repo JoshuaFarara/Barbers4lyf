866dfcf1216f726106194fceb8ba97b8654baeb3
JoshuaFarara <joshua.farara@gmail.com>
2024-07-07 14:26:41 -0400

    Changes to be committed: 	modified:   backend/Dockerfile 	modified:   backend/backend/__pycache__/settings.cpython-311.pyc 	modified:   backend/entrypoint.sh 	modified:   docker-compose.yaml 	modified:   frontend-ts/Dockerfile 	modified:   frontend-ts/package-lock.json 	modified:   helpfulResources.txt 	modified:   nginx/Dockerfile 	modified:   nginx/default.conf

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/ash
+set -e
 
-echo "Waiting for MySQL..."
-sleep 5
-echo "Apply database migrations"
+# Wait for the database to be ready
+# /wait-for-it.sh db:3306 --timeout=30 --strict -- echo "Database is up"
+
+# wait for database to be ready
+while ! nc -z db 3306; do
+  echo "Waiting for the database to be ready..."
+  sleep 5 
+done
 
 python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 echo "Database migrated"
 
-gunicorn backend.wsgi:application --bind0.0.0.0:8000
-
-exec"$@"
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000 "$@"
